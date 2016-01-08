4f5534db7a7fb39a08a104a8734e75ed9e6d3d1f
Chen Bingfeng <iichenbf@gmail.com>
2016-01-09 00:59:27 +0800

    a

--- Assets/Scripts/LayersController.cs
+++ Assets/Scripts/LayersController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LayersController : MonoBehaviour {
 
@@ -8,10 +9,13 @@ public class LayersController : MonoBehaviour {
 	private GameObject _content;
 	private int _layerIndex = 0;
 
+
+	private List<GameObject> _layerList;
+
 	// Use this for initialization
 	void Start () {
 		_content = GameObject.Find ("LayersContent");
-		Debug.Assert (_content);
+		_layerList = new List<GameObject> ();
 	}
 	
 	// Update is called once per frame
@@ -20,6 +24,7 @@ public class LayersController : MonoBehaviour {
 	}
 
 
+	private float _lineHeight = 0;
 	public void OnClickNewLayer()
 	{
 		Debug.Log ("new layer");
@@ -28,10 +33,20 @@ public class LayersController : MonoBehaviour {
 		// will 
 		int layerId = _layerIndex++;
 
-		var newLayerLine = Instantiate (_layerFab) as GameObject;
-		Debug.Assert (newLayerLine.transform != null);
-		newLayerLine.transform.SetParent (_content.transform);
+		var layer = Instantiate (_layerFab) as GameObject;
+		layer.transform.SetParent (_content.transform);
+		var rect = layer.GetComponent<RectTransform> ();
+		_lineHeight = rect.sizeDelta.y * 1.2f;
+		rect.localPosition = new Vector3 (0, -_lineHeight * _layerList.Count, 0);
 
+		_layerList.Add (layer);
+		RefreshContentSize ();
+	}
+
+	void RefreshContentSize()
+	{
+		var rect = _content.GetComponent<RectTransform> ();
+		rect.sizeDelta = new Vector2 (rect.sizeDelta.x, _layerList.Count * _lineHeight);
 	}
 
 	public void OnClickDeleteLayer()
