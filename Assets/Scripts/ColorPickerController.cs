3724d5b11c0b8e2dc11dc9ca5222096d452d060d
Chen Bingfeng <iichenbf@gmail.com>
2016-01-09 23:33:56 +0800

    add color picker

---
+++ Assets/Scripts/ColorPickerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorPickerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnSetColor(Color color)
+	{
+		Debug.Log ("on set color, " + color.ToString ());
+	}
+
+	public Color OnGetColor()
+	{
+		Debug.Log ("OnGetColor, ");
+		return Color.red;
+
+	}
+}
