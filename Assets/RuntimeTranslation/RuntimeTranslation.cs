925ffac7de102ec372fc77900ac426269a9be323
Chen Bingfeng <iichenbf@gmail.com>
2016-01-11 03:05:20 +0800

    runtime translation keys

--- Assets/RuntimeTranslation/RuntimeTranslation.cs
+++ Assets/RuntimeTranslation/RuntimeTranslation.cs
@@ -1,15 +1,98 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class RuntimeTranslation : MonoBehaviour {
-
+	public GameObject btn_move;
+	public GameObject btn_rotate;
+	public GameObject btn_scale;
+	
 	// Use this for initialization
 	void Start () {
-	
+		SetBtnSelection (btn_move, false);
+		SetBtnSelection (btn_rotate, false);
+		SetBtnSelection (btn_scale, false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown ("escape")) {
+			SetBtnSelection (btn_move, false);
+			SetBtnSelection (btn_rotate, false);
+			SetBtnSelection (btn_scale, false);
+		}
+	}
+
+	void SetBtnSelection(GameObject btn, bool isSelected)
+	{
+		btn.GetComponent<Outline> ().enabled = isSelected;
+		btn.GetComponent<Image> ().color = isSelected ? Color.gray : Color.white;
+	}
+
+	bool GetBtnSelection(GameObject btn)
+	{
+		return btn.GetComponent<Outline> ().enabled;
+	}
 	
+	public void OnClickBtnMove()
+	{
+
+		Debug.Log ("OnClickBtnMove");
+
+		{
+			var theBtn = btn_move;
+			if (GetBtnSelection (theBtn)) {
+				SetBtnSelection (theBtn, false);
+				// toggle off
+			} else {
+				// toggle on, and off others
+				// first, off all
+				SetBtnSelection(btn_move, false);
+				SetBtnSelection(btn_rotate, false);
+				SetBtnSelection(btn_scale, false);
+				// on the one
+				SetBtnSelection(theBtn, true);
+			}
+		}
+	}
+
+	public void OnClickBtnRotate()
+	{
+		Debug.Log ("OnClickBtnRotate");
+		{
+			var theBtn = btn_rotate;
+			if (GetBtnSelection (theBtn)) {
+				SetBtnSelection (theBtn, false);
+				// toggle off
+			} else {
+				// toggle on, and off others
+				// first, off all
+				SetBtnSelection(btn_move, false);
+				SetBtnSelection(btn_rotate, false);
+				SetBtnSelection(btn_scale, false);
+				// on the one
+				SetBtnSelection(theBtn, true);
+			}
+		}
+	}
+
+	public void OnClickBtnScale()
+	{
+		Debug.Log ("OnClickBtnScale");
+		{
+			var theBtn = btn_scale;
+			if (GetBtnSelection (theBtn)) {
+				SetBtnSelection (theBtn, false);
+				// toggle off
+			} else {
+				// toggle on, and off others
+				// first, off all
+				SetBtnSelection(btn_move, false);
+				SetBtnSelection(btn_rotate, false);
+				SetBtnSelection(btn_scale, false);
+				// on the one
+				SetBtnSelection(theBtn, true);
+			}
+		}
 	}
 }
