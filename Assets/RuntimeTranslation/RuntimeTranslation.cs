f797e0a7e8361186e88ffdee5eff3c49855c01e4
Chen Bingfeng <iichenbf@gmail.com>
2016-01-11 23:59:53 +0800

    gizmo show hide

--- Assets/RuntimeTranslation/RuntimeTranslation.cs
+++ Assets/RuntimeTranslation/RuntimeTranslation.cs
@@ -6,35 +6,58 @@ public class RuntimeTranslation : MonoBehaviour {
 	public GameObject btn_move;
 	public GameObject btn_rotate;
 	public GameObject btn_scale;
+
+	public GameObject gizmo_move;
+	public GameObject gizmo_rotate;
+	public GameObject gizmo_scale;
+
+	enum RTT {MOVE, ROTATE, SCALE};
 	
 	// Use this for initialization
 	void Start () {
-		SetBtnSelection (btn_move, false);
-		SetBtnSelection (btn_rotate, false);
-		SetBtnSelection (btn_scale, false);
+		SetBtnSelection(RTT.MOVE, false);
+		SetBtnSelection(RTT.SCALE, false);
+		SetBtnSelection(RTT.ROTATE, false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// escape to exit any edit mode
 		if (Input.GetKeyDown ("escape")) {
-			SetBtnSelection (btn_move, false);
-			SetBtnSelection (btn_rotate, false);
-			SetBtnSelection (btn_scale, false);
+			SetBtnSelection(RTT.MOVE, false);
+			SetBtnSelection(RTT.SCALE, false);
+			SetBtnSelection(RTT.ROTATE, false);
 		}
 
 		// update gizmo's scale, so when camera move, it always has the same vision size!!!
 		// TODO
 	}
 
-	void SetBtnSelection(GameObject btn, bool isSelected)
+	GameObject RTT2Btn(RTT t)
 	{
-		btn.GetComponent<Outline> ().enabled = isSelected;
-		btn.GetComponent<Image> ().color = isSelected ? Color.gray : Color.white;
+		return t == RTT.MOVE ? btn_move :
+			t == RTT.ROTATE ? btn_rotate : btn_scale;
 	}
 
-	bool GetBtnSelection(GameObject btn)
+	GameObject RTT2Gizmo(RTT t)
 	{
+		return t == RTT.MOVE ? gizmo_move :
+			t == RTT.ROTATE ? gizmo_rotate : gizmo_scale;
+	}
+
+	void SetBtnSelection(RTT t, bool isSelected)
+	{
+		var btn = RTT2Btn (t);
+		btn.GetComponent<Outline> ().enabled = isSelected;
+		btn.GetComponent<Image> ().color = isSelected ? Color.gray : Color.white;
+
+		var gizmo = RTT2Gizmo (t);
+		gizmo.SetActive (isSelected);
+	}
+
+	bool GetBtnSelection(RTT t)
+	{
+		var btn = RTT2Btn (t);
 		return btn.GetComponent<Outline> ().enabled;
 	}
 	
@@ -44,16 +67,16 @@ public class RuntimeTranslation : MonoBehaviour {
 		Debug.Log ("OnClickBtnMove");
 
 		{
-			var theBtn = btn_move;
+			var theBtn = RTT.MOVE;
 			if (GetBtnSelection (theBtn)) {
 				SetBtnSelection (theBtn, false);
 				// toggle off
 			} else {
 				// toggle on, and off others
 				// first, off all
-				SetBtnSelection(btn_move, false);
-				SetBtnSelection(btn_rotate, false);
-				SetBtnSelection(btn_scale, false);
+				SetBtnSelection(RTT.MOVE, false);
+				SetBtnSelection(RTT.SCALE, false);
+				SetBtnSelection(RTT.ROTATE, false);
 				// on the one
 				SetBtnSelection(theBtn, true);
 			}
@@ -64,16 +87,16 @@ public class RuntimeTranslation : MonoBehaviour {
 	{
 		Debug.Log ("OnClickBtnRotate");
 		{
-			var theBtn = btn_rotate;
+			var theBtn = RTT.ROTATE;
 			if (GetBtnSelection (theBtn)) {
 				SetBtnSelection (theBtn, false);
 				// toggle off
 			} else {
 				// toggle on, and off others
 				// first, off all
-				SetBtnSelection(btn_move, false);
-				SetBtnSelection(btn_rotate, false);
-				SetBtnSelection(btn_scale, false);
+				SetBtnSelection(RTT.MOVE, false);
+				SetBtnSelection(RTT.SCALE, false);
+				SetBtnSelection(RTT.ROTATE, false);
 				// on the one
 				SetBtnSelection(theBtn, true);
 			}
@@ -84,16 +107,16 @@ public class RuntimeTranslation : MonoBehaviour {
 	{
 		Debug.Log ("OnClickBtnScale");
 		{
-			var theBtn = btn_scale;
+			var theBtn = RTT.SCALE;
 			if (GetBtnSelection (theBtn)) {
 				SetBtnSelection (theBtn, false);
 				// toggle off
 			} else {
 				// toggle on, and off others
 				// first, off all
-				SetBtnSelection(btn_move, false);
-				SetBtnSelection(btn_rotate, false);
-				SetBtnSelection(btn_scale, false);
+				SetBtnSelection(RTT.MOVE, false);
+				SetBtnSelection(RTT.SCALE, false);
+				SetBtnSelection(RTT.ROTATE, false);
 				// on the one
 				SetBtnSelection(theBtn, true);
 			}
